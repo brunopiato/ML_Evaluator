65871e65a1b1c589f387256518ac19a67f1131a7
brunopiato <piatobio@gmail.com>
2023-08-21 14:19:40 -0300

    update home text

--- Home.py
+++ Home.py
@@ -20,7 +20,7 @@ st.sidebar.markdown('#### by Bruno Piato')
 st.markdown("""
             # Machine Learning Evaluator
             
-            This WebApp was developed to bring to life the final essay of the Machine Learning Fundamentals discipline. Its main goal is to provide a visual implementation of how the different hyperparameters of some of the main machine learning algorithms impact their performance metrics. 
+            This WebApp was developed to bring to life the final essay of the Machine Learning Fundamentals discipline. Its main goal is to provide a visual implementation of how the different hyperparameters of some of the main machine learning algorithms impact their performance metrics. All the algorithms of each of the three types were trained, validated and tested using the exact same dataset so the observed differences area only due to algorithms functioning.
             
             Here I implemented a few of the ML algorithms we used during the discipline:
             - Classification algorithms:
@@ -38,7 +38,9 @@ st.markdown("""
                 - K-Means
                 - Affinity Propagation
 
-            As the algorithms are rerun everytime the user changes the hyperparameter, it can time a little to the results be shown, so please, be patient. 
+            At the last tab of each page there is a model comparison of the algorithms as the user set them. 
+            
+            As all the algorithms are rerun everytime the user changes a hyperparameter, it can time a little to the results be shown, so please, be patient. 
             
             If you have any question, suggestion, issue report and critics, please let me know by reaching me through [e-mail](mailto:piatobio@gmail.com) or by [Discord](https://discordapp.com/users/438408418429239296).
             """)
